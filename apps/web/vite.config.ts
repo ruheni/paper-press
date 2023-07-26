dc1afad15d5da6112428047329547e2aded23944
cskiwi <glenn.latomme@gmail.com>
2023-07-26 09:30:49 +0200

    fix: should fix the nitro build

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -24,6 +24,9 @@ export default defineConfig(({ mode }) => {
         prerender: {
           routes: ['/'],
         },
+        nitro: {
+          preset: 'vercel'
+        }          
       }),
       tsConfigPaths({
         root: '../../',
